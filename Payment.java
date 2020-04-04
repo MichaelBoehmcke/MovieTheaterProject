5e0a2cfd3e48381c1ae286df22a1cbde7f875cf4
mvischer1 <59977016+mvischer1@users.noreply.github.com>
2020-04-04 14:28:29 -0400

    Add files via upload

--- Payment.java
+++ Payment.java
@@ -7,6 +7,7 @@ public class Payment {
   private long cardNum;
   private int cvv;
   
+  //Basic constructor for users to create a new payment
   public Payment(String aCardType, String aName, String anExpDate, long aCardNum, int aCvv) {
     this.cardType = aCardType;
     this.name = aName;
@@ -15,6 +16,7 @@ public class Payment {
     this.cvv = aCvv;
   }
   
+  //to be used in recipt class or account information
   public String toString() {
     return "Card Type: "+this.cardType+"\nName: "+this.name+"\nExp Date: "+this.expDate+
       "\nCard Number: "+this.cardNum+"\nCVV: "+this.cvv;
--- Reciept.java
+++ Reciept.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 import java.io.*;
 
+//Contains the payment used and a random reciept id between 1-10000
 public class Reciept {
   private Payment payment;
   private int id;
@@ -9,9 +10,13 @@ public class Reciept {
     this.payment = aPayment;
     this.id = r.nextInt(10000);
   }
+  
+  //for printing to console
   public String toString() {
     return "Reciept ID: "+this.id+"\nPayment Information: "+this.payment.toString();
   }
+  
+  //prints to txt file, TODO! need to specify location
   public void printReciept() {
     try {
     String fileContent = "Reciept ID: "+this.id+"\nPayment Information: "+payment.toString()+

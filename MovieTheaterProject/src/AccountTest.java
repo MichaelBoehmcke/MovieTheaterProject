f8b11bba4ceac89b9e700897d5f9205e3a0d0c9f
Alden Perreyclear <47489090+Perreyclear@users.noreply.github.com>
2020-04-13 04:48:33 -0400

    Added tests on some classes

---
+++ MovieTheaterProject/src/AccountTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void testChangeUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testChangePassword() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ MovieTheaterProject/src/Administrator.java
@@ -0,0 +1,14 @@
+
+public class Administrator extends Account{
+
+	public Administrator() {
+		super.role="Admin";
+		super.username="AdminDefault";
+		super.password="admin1";
+	}
+	
+	public String toString() {
+		return super.toString();
+	}
+	
+}

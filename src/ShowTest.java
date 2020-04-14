dea5d58c144c433b7805c56615c6e4f446a59e5c
Michael Boehmcke <michaelboehmcke@icloud.com>
2020-04-14 14:23:31 -0400

    Merging

---
+++ src/ShowTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+public class ShowTest {
+	
+	@BeforeClass
+	public static void oneTimeSetup() {
+		
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() {
+		
+	}
+	
+	@Before
+	public static void setup() {
+		//runs before each test
+	}
+	
+	@After
+	public static void tearDown() {
+		//runs after each test
+	}
+
+}
